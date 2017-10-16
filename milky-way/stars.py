8c8bb7d4dcd3e8af1746fae21c4caea40c46f64c
Calvin Sykes <sykescalvin09@gmail.com>
2017-10-16 23:26:56 +0100

    Added 6d cartesian output

--- milky-way/stars.py
+++ milky-way/stars.py
@@ -5,7 +5,8 @@ import astropy as ap
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
-from astropy.coordinates import SphericalDifferential
+from astropy.coordinates import Galactocentric
+from astropy.coordinates import ICRS
 
 import matplotlib.pyplot as plt
 
@@ -21,19 +22,28 @@ import matplotlib.pyplot as plt
 def stars(infile):
     # Create structured datatype correspond to given format
     # and load data file
-    record = np.dtype([('ra'  , 'f'),
+    intype = np.dtype([('ra'  , 'f'),
                       ('dec'  , 'f'),
                       ('d'    , 'f'),
                       ('vlos' , 'f'),
                       ('pmra' , 'f'),
                       ('pmdec','f')])
-    data = np.loadtxt(infile,dtype=record,skiprows=1)
-
-    # This converts the input data to Cartesian position components
-    data_gcen = to_gcen(data)
+    data = np.loadtxt(infile,dtype=intype,skiprows=1)
 
     # astropy.SkyCoords doesn't support doing the velocity components though
     # so we need to do these manually
+    icrs = ICRS(ra=data['ra'] * u.degree,
+                dec=data['dec'] * u.degree,
+                distance=data['d'] * u.kpc,
+                pm_ra_cosdec=data['pmra'] * u.mas / u.yr * np.cos(data['dec']),
+                pm_dec=data['pmdec'] * u.mas / u.yr,
+                radial_velocity=data['vlos'] * u.km / u.s)
+    gal_cen = icrs.transform_to(Galactocentric)
+
+    out_data = np.column_stack((gal_cen.x, gal_cen.y, gal_cen.z, gal_cen.v_x, gal_cen.v_y, gal_cen.v_z))
+    cols = ('x[kpc]', 'y[kpc]', 'z[kpc]', 'v_x[km/s]', 'v_y[km/s]', 'v_z[km/s]')
+    delim = ' '# * 6
+    np.savetxt('6d.txt', out_data, fmt='%9.4f', delimiter=delim, header=delim.join(cols))    
 
     # plot positions
     from matplotlib import rc
@@ -41,14 +51,8 @@ def stars(infile):
     fig = plt.figure()
     ax = plt.gca()
     ax.set_aspect('equal')
-    plt.scatter(data_gcen.x,data_gcen.y) 
-    plt.show()
-
-def to_gcen(data):
-    return SkyCoord(ra=data['ra'] * u.degree,
-                    dec=data['dec'] * u.degree,
-                    distance=data['d'] * u.kpc,
-                    frame='icrs').galactocentric
+    #plt.scatter(gal_cen.x,gal_cen.y) 
+    #plt.show()
 
 if __name__ == '__main__':
     import argparse
