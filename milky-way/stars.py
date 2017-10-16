4c47ec153217971adbe8fdb6eb5752bb356d1834
Calvin Sykes <sykescalvin09@gmail.com>
2017-10-16 21:25:37 +0100

    Initial implementation of Milky Way homework (WIP)

---
+++ milky-way/stars.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# Import statements
+import numpy as np
+import astropy as ap
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import SphericalDifferential
+
+# given RA, dec, distance from Sun D, proper motions in RA and dec
+
+# 1) Convert to 6D cartesian coordinates
+# - Convert from RA, dec to galactic coordinates
+# - Convert to heliocentric Cartesian coordinates
+# - Move origin to get galactocentric coordinates
+# This sorts positions, how to get velocities?
+
+def stars(infile):
+    # Create structured datatype correspond to given format
+    # and load data file
+    record = np.dtype([('ra'  , 'f'),
+                      ('dec'  , 'f'),
+                      ('d'    , 'f'),
+                      ('vlos' , 'f'),
+                      ('pmra' , 'f'),
+                      ('pmdec','f')])
+    data = np.loadtxt(infile,dtype=record,skiprows=1)
+
+    # This converts the input data to Cartesian position components
+    data_gcen = to_gcen(data)
+
+    # astropy.SkyCoords doesn't support doing the velocity components though
+    # so we need to do these manually
+
+    print(data_gcen)
+
+def to_gcen(data):
+    return SkyCoord(ra=data['ra'] * u.degree,
+                    dec=data['dec'] * u.degree,
+                    distance=data['d'] * u.kpc,
+                    frame='icrs').galactocentric
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',type=str)
+    args = parser.parse_args()
+    
+#    np.random.seed(42)
+    
+    stars(args.filename)
